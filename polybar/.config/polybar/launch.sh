12a631dcb6a005b76fcb81eefe94522095788339
Chris Boeh <cboeh21@gmail.com>
2017-10-22 16:23:07 -0500

    Added i3-gaps and some polybar changes

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,7 +1,7 @@
 killall -q polybar
 
 while pgrep -x polybar >/dev/null/; do sleep 1; done
-
-polybar example &
+MONITOR=DVI-I-1 polybar example &
+MONITOR=HDMI-1 polybar example &
 
 echo "Bars launched..."
