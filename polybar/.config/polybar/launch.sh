ade17a0a3bc1646135c86dca198169df92e6a9f4
root <root@Arch.univ.missouriwestern.edu>
2017-10-23 15:31:29 -0500

    Changes to polybar and addition of Feh

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,5 +3,5 @@ killall -q polybar
 while pgrep -x polybar >/dev/null/; do sleep 1; done
 MONITOR=DVI-I-1 polybar example &
 MONITOR=HDMI-1 polybar example &
-
+MONITOR=eDP1 polybar example &
 echo "Bars launched..."
