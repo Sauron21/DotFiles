636f95daf6044ae8a2dda4858b9f9fa569ba1b49
Sauron21 <cboeh21@gmail.com>
2018-01-23 12:00:20 -0600

    install script improvements

--- install_packages.sh
+++ install_packages.sh
@@ -6,7 +6,7 @@ install-packages() {
 	#applications
 	packages+='firefox '
 	#utilities
-	packages+='yaourt zsh ranger termite git tlp powertop stow gucharmap '
+	packages+='yaourt zsh ranger termite git tlp powertop stow gucharmap vim '
 	#services
 	packages+='wpa_supplicant dialog xorg-xbacklight ntp feh
 	xf86-input-synaptics xf86-video-intel '
@@ -16,24 +16,39 @@ install-packages() {
 	packages+='zsh-completions zsh-syntax-highlighting '
 	sudo pacman -Sy --noconfirm $packages
 }
+
 install-packages-yaourt() {
 	local packagesyaourt=''
 	#dektop enviroment
-	packagesyaourt+='polybar '
+	packagesyaourt+='polybar siji '
 	yaourt -Sy --noconfirm $packagesyaourt
 }
+
 start-daemons() {
 	local daemons=''
-	daemons+='ntpdate.service lightdm.service '
+	daemons+='ntpdate.service lightdm.service tlp.service tlp-sleep.service '
+	sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
 	sudo systemctl enable $daemons
 	sudo systemctl start $daemons
 }
+
 dotfiles() {
-	###git clone https://github.com/Sauron21/DotFiles
+	git clone https://github.com/Sauron21/DotFiles
 	stow Ranger i3 polybar vim xorg zsh termite
+	sudo stow -t / laptop
+}
+
+setup-user-accounts() {
+	#set root password and create new user account
+	passwd
+	redeyes1
+	useradd -m -g users -G wheel -s /bin/bash chris
+	passwd chris
+	redeyes1
 }
 
 timedatectl set-ntp true
+setup-user-accounts
 install-packages
 install-packages-yaourt
 dotfiles
@@ -42,3 +57,4 @@ start-daemons
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #change shell to zsh
 chsh -s /bin/zsh
+
