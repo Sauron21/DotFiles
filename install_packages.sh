811a817dc4c768ea5c30b9b9b347248d75f51a1c
Sauron21 <cboeh21@gmail.com>
2018-02-25 15:11:28 -0600

    Various Changes

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 install-packages() {
 	local packages=''
 	#fonts
@@ -7,11 +8,11 @@ install-packages() {
 	packages+='firefox libreoffice-fresh '
 	#utilities
 	packages+='yaourt zsh ranger termite git tlp powertop stow gucharmap vim
-	pavucontrol '
+	pavucontrol cups '
 	#services
 	packages+='wpa_supplicant dialog xorg-xbacklight ntp feh
 	xf86-input-synaptics xf86-video-intel compton wpa_actiond wireless_tools
-	pulseaudio '
+	pulseaudio lightdm lightdm-gtk-greeter avahi hplip '
 	#desktop enviroment
 	packages+='xorg-server xorg-xinit i3-gaps dmenu '
 	#addons
@@ -29,16 +30,21 @@ install-packages-yaourt() {
 start-daemons() {
 	local daemons=''
 	daemons+='ntpdate.service lightdm.service tlp.service tlp-sleep.service
-	netctl-auto@wl01.service '
+	netctl-auto@wlo1.service org.cups.cupsd.service avahi-daemon.service '
 	sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
 	sudo systemctl enable $daemons
 	sudo systemctl start $daemons
 }
 
-timedatectl set-ntp true
+dotfiles() {
+	stow Ranger i3 polybar vim xorg zsh termite
+	sudo stow -t / touchpad
+}
+
 install-packages
 install-packages-yaourt
 start-daemons
+timedatectl set-ntp true
 #oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #change shell to zsh
